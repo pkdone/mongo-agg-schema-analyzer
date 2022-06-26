f9adc5d0354384c2429a919cf3bcd0d583f8b811
Paul Done <pkdone@gmail.com>
2022-06-26 10:55:26 +0100

    Added test shell script

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 # To execute first export URL pointing at a running MongoDB deployment
-# export URL="mongodb+srv://main_user:Password1@aggcluster.s703u.mongodb.net/test"
+# export URL="mongodb+srv://myuser:mypasswd@mycluster.abc.mongodb.net/test"
 
 DEFAULT_URL="mongodb://localhost:27017/test"
 : "${URL:=${DEFAULT_URL}}"
